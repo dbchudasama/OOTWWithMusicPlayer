9283c70655f1dca9f598dfc1902801618f1ed8d4
DivyeshBC <DivXMacBookPro@Divyeshs-MacBook-Pro.local>
2014-07-01 14:12:05 +0100

    Initial Commit

---
+++ Out of the World/OWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWViewController.h
+//  Out of the World
+//
+//  Created by DivyeshBC on 01/07/2014.
+//  Copyright (c) 2014 DivyeshBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWViewController : UIViewController
+
+@end
---
+++ Out of the World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Out of the World
+//
+//  Created by DivyeshBC on 01/07/2014.
+//  Copyright (c) 2014 DivyeshBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OWAppDelegate class]));
+    }
+}
